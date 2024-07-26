72215ed27a2d66cbf82f5a7b6dbdb81993b7d313
LuisAlbertoAttack1 <laaolivares837@gmail.com>
2024-07-26 12:11:32 -0600

    NuevoProyecto

---
+++ public/js/mascaraDeCarga/mascara.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    mascaraObloqueo();
+});
+
+function mascaraObloqueo() {
+    $.blockUI({
+        //AQUI SOLO CAMBIA LA IMG Y COLOCA EL ORDEN DE TUS CARPETAS PARA QUE FUNCIONE BIEN
+        //Nota: Si Tienes Tu Carpeta De Img En Una Carpeta Public Puede Causar Fallas De Que No Agarre La Img Solo Saca La Carpeta Img De La Carpeta Publica Y Listo
+        message: ' <img src="../../../public/Img/Logos/Logo.png" class="img-fluid" width="300" height="300">',
+        css: {
+            border: 'none',
+            // padding: '15px', 
+            backgroundColor: 'rgba(138,221,45,0)',
+            '-webkit-border-radius': '10px',
+            '-moz-border-radius': '10px',
+            // opacity: .5, 
+            color: '#FFFFFF '
+        }
+    });
+
+    setTimeout($.unblockUI, 1200);
+    
+}
+
+	
+new DataTable('#example');
