114c1fb6e923c834731165b4212f8ff3f3858c93
LuisAlbertoAttack1 <laaolivares837@gmail.com>
2024-07-26 12:14:43 -0600

    NuevoProyecto

--- public/js/mascaraDeCarga/mascara.js
+++ public/js/mascaraDeCarga/mascara.js
@@ -6,7 +6,7 @@ function mascaraObloqueo() {
     $.blockUI({
         //AQUI SOLO CAMBIA LA IMG Y COLOCA EL ORDEN DE TUS CARPETAS PARA QUE FUNCIONE BIEN
         //Nota: Si Tienes Tu Carpeta De Img En Una Carpeta Public Puede Causar Fallas De Que No Agarre La Img Solo Saca La Carpeta Img De La Carpeta Publica Y Listo
-        message: ' <img src="../../../public/Img/Logos/Logo.png" class="img-fluid" width="300" height="300">',
+        message: ' <img src="https://luisalbertoattack1.github.io/PracticaExamen/public/Img/Logos/Logo.png" class="img-fluid" width="300" height="300">',
         css: {
             border: 'none',
             // padding: '15px', 
